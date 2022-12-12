cb554fdec979b15072b9358a93097463b0a65c5b
Arc-Data <arclight04@gmail.com>
2022-12-12 16:07:40 +0800

    added js skeleton

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+class Book {
+	constructor(title, author, readStatus) {
+		this.title = title;
+		this.author = author;
+		this.readStatus = readStatus;
+	}
+}
+
+function addBookToLibrary() {
+	
+}
