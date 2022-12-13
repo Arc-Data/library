6ab41087eb95fc20925522c99d55b530f0e698d7
Arc-Data <arclight04@gmail.com>
2022-12-13 14:00:47 +0800

    open and close modal form

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const openFormBtn = document.querySelector('#openForm');
+const closeFormBtn = document.querySelector('.close-btn')
+const overlay = document.querySelector('#overlay');
+const modalForm = document.querySelector('.modal-form');
 let myLibrary = [];
+
+
 
 class Book {
 	constructor(title, author, readStatus) {
@@ -11,3 +17,20 @@ class Book {
 function addBookToLibrary() {
 	
 }
+
+
+function openForm() {
+	modalForm.classList.add('active');
+	overlay.classList.add('active');
+}
+
+function closeForm(e) {
+	
+	modalForm.classList.remove('active');
+	overlay.classList.remove('active');
+}
+
+
+overlay.addEventListener('click', closeForm);
+closeFormBtn.addEventListener('click', closeForm);
+openFormBtn.addEventListener('click', openForm);
